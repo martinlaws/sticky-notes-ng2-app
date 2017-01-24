cadd72116587149f37082155fece8561dacfe377
Martin Laws <martin.laws@indexexchange.com>
2017-01-24 16:00:09 -0500

    Initial commit/setup

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '',
+  template: `
+    <div>
+      <h3>
+        Hello, World!
+      </h3>
+    </div>
+  `
+})
+
+export class App {};
