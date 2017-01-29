42e9d7644a9632302e5d90f2b0f0673e9f6e6185
Martin Laws <martin.laws@indexexchange.com>
2017-01-28 22:46:02 -0500

    Fully integrated data store

--- src/app/services/notes.ts
+++ src/app/services/notes.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api';
+import { StoreHelper } from './store-helper';
 import 'rxjs/Rx';
 
 @Injectable()
 export class NoteService {
   path: string = '/notes';
-  constructor(private apiService: ApiService) {
-
-  }
+  constructor(
+      private apiService: ApiService,
+      private storeHelper: StoreHelper
+  ) {  }
 
   createNote(note) {
-    return this.apiService.post(this.path, note);
+    return this.apiService.post(this.path, note)
+      .do(savedNote => this.storeHelper.add('notes', savedNote));
   }
 
   getNotes() {
-    let savedNotes = this.apiService.get(this.path);
-    return savedNotes;
+    return this.apiService.get(this.path)
+      .do((resp: any)=> this.storeHelper.update('notes', resp.data));
   }
 
   completeNote(note) {
-    return this.apiService.delete(`${this.path}/${note.id}`);
+    return this.apiService.delete(`${this.path}/${note.id}`)
+      .do((resp: any) => this.storeHelper.findAndDelete('notes', resp.id));
   }
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -25,7 +25,7 @@ const _store = new BehaviorSubject<State>(defaultState);
 @Injectable()
 export class Store {
   private store = _store;
-  changes = this.store.asObservable().distinctUntilChanged()
+  changes = this.store.asObservable().distinctUntilChanged();
 
   setState(state: State) {
     this.store.next(state);
